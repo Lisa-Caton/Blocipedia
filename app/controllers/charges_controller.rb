8f6cb693c2fabaf6a58428b30aa2e7551c638490
Lisa Caton <lisacaton01@gmail.com>
2018-08-28 16:00:19 -0400

    Upgrading stripe code

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,6 +3,11 @@ class ChargesController < ApplicationController
   # before_filter :authenticate_user!
 
   def new
+    @stripe_btn_data = {
+    :key => "#{ Rails.configuration.stripe[:publishable_key] }",
+    :description => "BigMoney Membership - #{current_user.name}",
+    :amount => @amount
+  }
   end
 
   def create
@@ -19,9 +24,12 @@ class ChargesController < ApplicationController
     charge = Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => @amount,
-      :description => 'Rails Stripe customer',
+      :description => 'BigMoney Membership - #{current_user.email}',
       :currency    => 'usd'
     )
+
+    flash[:notice] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
+   redirect_to wikis_path(current_user) # or wherever
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
