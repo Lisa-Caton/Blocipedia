bf774391615a97dc94828243904642776b0b4dd8
Lisa Caton <lisacaton01@gmail.com>
2018-09-06 10:26:08 -0400

    adding create to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
 
+  def create
+    @user = User.new
+    @user.username = params[:user][:username]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save
+      flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+      create_session(@user)
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was an error creating your account. Please try again."
+      render :new
+    end
+  end
+
   def premium_to_standard
     current_user.update_attributes(role: 'standard')
     redirect_to edit_user_registration_path
